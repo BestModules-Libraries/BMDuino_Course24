121aceee3a1faa12823e3d4eb21570c26879143b
prgbruce@gmail.com <prgbruce@gmail.com>
2025-06-20 14:09:41 +0800

    commit all

--- TCP_BCOM2/TCP_BCOM2.ino
+++ TCP_BCOM2/TCP_BCOM2.ino
@@ -8,9 +8,9 @@ Description: 1. Hardware serial port 1 is used for communication with BMC81M001,
 #include "TCP.h" // 引入 TCP 庫
 
 //BMC81M001 Wifi(6,7);// 使用軟體序列（rxPin 6 , txPin 7），若不使用軟體序列請註解掉這行
-BMC81M001 Wifi(&Serial); // 使用硬體序列0（HW Serial），若使用 BMduino 的硬體序列，請取消這行的註解
+//BMC81M001 Wifi(&Serial); // 使用硬體序列0（HW Serial），若使用 BMduino 的硬體序列，請取消這行的註解
 //BMC81M001 Wifi(&Serial1); // 使用硬體序列1（HW Serial1），若使用 BMduino 的硬體序列，請取消這行的註解
-//BMC81M001 Wifi(&Serial2); // 使用硬體序列2（HW Serial2），若使用 BMduino 的硬體序列，請取消這行的註解
+BMC81M001 Wifi(&Serial2); // 使用硬體序列2（HW Serial2），若使用 BMduino 的硬體序列，請取消這行的註解
 //BMC81M001 Wifi(&Serial3); // 使用硬體序列3（HW Serial3），若使用 BMduino 的硬體序列，請取消這行的註解
 //BMC81M001 Wifi(&Serial4); // 使用硬體序列4（HW Serial4），若使用 BMduino 的硬體序列，請取消這行的註解
 
